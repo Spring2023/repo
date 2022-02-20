fccbf38b77b9e191dd32139217ba9262ac60a9d9
Spring2023 <kristinka.beresneva@yandex.ru>
2022-02-20 16:05:07 +0300

    My HW to L1

---
+++ HW_L1.py
@@ -0,0 +1,93 @@
+#1. попробовать print
+print ("Hello World")
+print (123)
+print ("рроо")
+print ("Hello 1")
+print ()
+       
+#2.1 вывести Programming***Essentials***in...Python
+print ("Programming", "Essentials", "in", sep="***", end="...")
+print ("Python")
+
+#2.2 вывести стрелку
+print ("    ", "   * ", "  *   ", " *     *", sep="*\n")
+print ("***   ***", "  *   *", "  *   *", "  *****", sep="\n")
+
+#2.3 вывести в 3 строки
+print ("\"I'm\"", "\"\"learning\"\"", "\"\"\"Python\"\"\"", sep="\n")
+
+#2.4 посчитать сумму яблок
+name="john"
+name1="mary"
+name2="adam"
+john=2
+mary=3
+adam=4
+print (john, mary, adam, sep=",")
+total_apples=(john+mary+adam)
+print (total_apples)
+print ("Total number of apples:", total_apples)
+
+#2.5 перевести км в мили и наоборот
+kilometers=12.25
+miles=7.38
+x=miles*1.61
+y=kilometers/1.61
+print (miles, "miles is", x, "kilometers")
+print (kilometers, "kilometers is", y, "miles")
+
+#2.6 вывести у под каждый х
+x=0.
+y=3*x**3 - 2*x**2 + 3*x - 1
+print (y)
+x=1.
+y=3*x**3 - 2*x**2 + 3*x - 1
+print (y)
+x=-1.
+y=3*x**3 - 2*x**2 + 3*x - 1
+print (y)
+
+#2.7 вывести секунд в часах
+a=2
+seconds=3600
+b=a*seconds
+print ("Hours:", a)
+print ("Seconds in hours:", b)
+
+#2.8 калькулятор
+x=int(input("Введите число "))
+y=int(input("Введите число "))
+print("Сумма =", x+y)
+print("Результат деления =", x/y)
+
+#2.9 посчитать дробь
+x = 1
+y = (1/((x+1)/((x+1)/(x+1/x))))
+print(y)
+
+#2.10 вывести hello world необычной программой
+print ("H", "e", "l", "l", "o ", "World", sep="", end="!")
+
+#2.11 вывести результат выражения
+a = 11111
+b = 1111111
+print (a*b)
+
+#2.12 результат дроби
+x = 42/(4+2*(-2))
+print (x)
+
+#2.13 возвести в степень
+x = 2014**14
+print (x)
+
+#2.14 секунд в месяце
+month = 30
+day = 24
+hour = 3600
+print (month*day*hour)
+
+#2.15 возвести в степень
+x = 2014.0**14
+print (x)
+
